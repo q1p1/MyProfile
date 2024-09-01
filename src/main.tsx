7013139601a7f424e73b61962351031db21ad385
q1p1 <meshari.algahbban@wakecap.com>
2024-09-01 16:11:23 +0300

    رسالة توضح التغييرات

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import ExperiencePage from "./Components/pages/ExperiencePage";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
+//
 root.render(
   <React.StrictMode>
     <Router>
