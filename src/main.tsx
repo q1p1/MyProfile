fe722af50efd5d8866c0990133d37767ce1de435
q1p1 <meshari.algahbban@wakecap.com>
2024-09-01 16:08:12 +0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-// index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import ExperiencePage from './Components/pages/ExperiencePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import ExperiencePage from "./Components/pages/ExperiencePage";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
