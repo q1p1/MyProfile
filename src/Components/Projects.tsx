ce6f5961368ddf49158ac06c0d2b2eb181b0c8f6
q1p1 <meshari.algahbban@wakecap.com>
2024-09-03 14:16:57 +0300

    main

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,31 +1,68 @@
+import imgmap from "../assets/MAPS.png";
+import github from "../assets/github.png";
+import QR from "../assets/qr.png";
+import task from "../assets/TASK.png";
 
 const projectsData = [
   {
-    title: " Projects 1",
-    description: "#########",
+    title: "Workers in maps",
+    description:
+      "The project is based on identifying workers on the map and calculating the number shown on the screen, and there are other characteristics that you can learn about when entering the project page.",
+    imageUrl: imgmap,
+    githubUrl: "https://github.com/AhmedAlaaEzzt/mk-todo-list",
   },
   {
-    title: "  Projects 1",
-    description: "#######",
+    title: "QR Code",
+    description: "Convert links and text into QR Code",
+    imageUrl: QR,
+    projectUrl: "https://github.com/q1p1/QR_code-fainal",
+    githubUrl: "https://qr-code-fainal.vercel.app/",
   },
   {
-    title: "  Projects 3",
-    description: "#######",
+    title: "Task",
+    description: "To save tasks and write new tasks",
+    imageUrl: task,
+    projectUrl: "https://todo-final-u9ro.vercel.app/",
+    githubUrl: "https://github.com/q1p1/todo-final",
   },
 ];
 
 function Projects() {
   return (
     <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">المشاريع</h2>
+      <h2 className="text-2xl font-bold mb-4">My Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projectsData.map((project, index) => (
           <div
             key={index}
             className="bg-white shadow-md rounded-md p-4 hover:shadow-lg transition-shadow duration-300"
           >
+            <a
+              href={project.projectUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={project.imageUrl}
+                alt={project.title}
+                className="w-full h-48 object-cover rounded-md mb-4"
+              />
+            </a>
             <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-            <p className="text-gray-600">{project.description}</p>
+            <p className="text-gray-600 mb-4">{project.description}</p>
+            <a
+              href={project.githubUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-600 hover:text-gray-900 flex items-center"
+            >
+              <img
+                src={github}
+                className="w-8 h-8 object-cover rounded-md mr-2"
+                alt="GitHub"
+              />
+              View on GitHub
+            </a>
           </div>
         ))}
       </div>
